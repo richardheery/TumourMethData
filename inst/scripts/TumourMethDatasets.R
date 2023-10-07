864f9561d7468aee937986ffa486731b8d7780bd
richardheery <richardheery@gmail.com>
2023-10-07 17:35:29 +0200

    Added dataset size to TumourMethDatasets

--- inst/scripts/TumourMethDatasets.R
+++ inst/scripts/TumourMethDatasets.R
@@ -1,11 +1,12 @@
 TumourMethDatasets = data.frame(
   dataset_name = c("cpgea_wgbs_hg38", "tcga_wgbs_hg38", "mcrpc_wgbs_hg38", "cao_esophageal_wgbs_hg19"), 
+  cancer_type = c("prostate", "various", "prostate", "esophageal"),
   technology = c("WGBS", "WGBS", "WGBS", "WGBS"),
   genome_build = c("hg38", "hg38", "hg38", "hg19"),
-  cancer_type = c("prostate", "various", "prostate", "esophageal"),
   number_tumour_samples = c(187, 39, 100, 10), 
   number_normal_samples = c(187, 8, 0, 9),
   coverage_available = c(FALSE, FALSE, TRUE, FALSE),
+  dataset_size_gb = c(40, 5.4, 16, 2),
   notes = c("", "", "", ""), 
   original_publication = c(
     "A genomic and epigenomic atlas of prostate cancer in Asian populations; Nature; 2020", 
